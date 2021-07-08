e96967264be8b00836ec43fff0b3ba6d5a210fce
kalunik <nikitaex123@gmail.com>
2021-07-08 03:04:43 +0300

    some problems w/h 'u' now there is pointer to structure working at printing after first founded conversion

--- ft_printf.c
+++ ft_printf.c
@@ -6,50 +6,50 @@
 //		width:			'*'
 //		precision:		'.'
 
-void	ft_var_output(t_conf_parser var, va_list arg_ptr)
+void	ft_var_output(t_conf_parser *var, va_list arg_ptr)
 {
 	char			c;
 	char			*s;
 	unsigned long	p;
 	long long		d;
 
-	if (var.type == 'c')
+	if (var->type == 'c')
 		ft_putchar_fd(va_arg(arg_ptr, int), 1);
-	else if (var.type == 's')
+	else if (var->type == 's')
 		ft_putstr_fd(va_arg(arg_ptr, char *), 1);
-	else if (var.type == 'p')
+	else if (var->type == 'p')
 	{
 		ft_putstr_fd("0x", 1);
 		ft_puthex_fd(va_arg(arg_ptr, unsigned long), 1);
 	}
-	else if (var.type == 'd' || var.type == 'i')
+	else if (var->type == 'd' || var->type == 'i')
 		ft_putnbr_fd(va_arg(arg_ptr, int), 1);
-	else if (var.type == 'u')
+	else if (var->type == 'u') // FIXME input 2147483648 -- output -2147483648
 		ft_putnbr_fd(va_arg(arg_ptr, unsigned int), 1);
-	else if (var.type == 'x')
+	else if (var->type == 'x')
 		ft_puthex_fd(va_arg(arg_ptr, unsigned int), 1);
-	else if (var.type == 'X')
+	else if (var->type == 'X')
 		ft_puthex_up_fd(va_arg(arg_ptr, unsigned int), 1);
-	else if (var.type == '%')
+	else if (var->type == '%')
 		ft_putstr_fd("%", 1);
 }
 
-t_conf_parser	ft_wdth_prcsn(char *conv, t_conf_parser var, va_list arg_ptr)
+void	ft_wdth_prcsn(char *conv, t_conf_parser *var, va_list arg_ptr)
 {
 	va_list arg_clone;
 
 	while (*conv == '-' || *conv == '0')
 		conv++;
-	if (*conv != '.' || *conv != var.type)
+	if (*conv != '.' || *conv != var->type)
 	{
 		if (*conv == '*') //TODO delete '*' if useless in mandatory part
 		{
-			va_copy(arg_clone, arg_ptr);
-			va_arg(arg_ptr,int);
-			var.width = va_arg(arg_ptr,int);
+			/*va_copy(arg_clone, arg_ptr);
+			va_arg(arg_ptr,int);*/
+			var->width = va_arg(arg_ptr,int);
 		}
 		else if (ft_isdigit(*conv))
-			var.width = ft_atoi(conv);
+			var->width = ft_atoi(conv);
 	}
 	while (*conv != '.')
 		conv++;
@@ -57,48 +57,45 @@ t_conf_parser	ft_wdth_prcsn(char *conv, t_conf_parser var, va_list arg_ptr)
 	{
 		conv++;
 		if (*conv == '*') //TODO delete '*' if useless in mandatory part
-			var.precision = va_arg(arg_ptr,int);
+			var->precision = va_arg(arg_ptr,int);
 		else if (ft_isdigit(*conv))
-			var.precision = ft_atoi(conv);
+			var->precision = ft_atoi(conv);
 	}
-	printf(" '%d' - '%d' - '%s'",  var.width, var.precision, conv);
-	return (var);
+	//printf(" '%d' - '%d' - '%s'",  var.width, var.precision, conv);
 }
 
-t_conf_parser	ft_flag_conf(char *conv, t_conf_parser var)
+void	ft_flag_conf(char *conv, t_conf_parser *var)
 {
-	var.flags = FLG_NONE;
 	while (*conv == '-' || *conv == '0')
 	{
 		if (*conv == '-')
 		{
-			var.flags |= FLG_MINUS;
-			var.flags &= ~FLG_ZERO;
+			var->flags |= FLG_MINUS;
+			var->flags &= ~FLG_ZERO;
 		}
-		else if (*conv == '0' && (var.flags << 7) != 128)
-			var.flags |= FLG_ZERO;
+		else if (*conv == '0' && (var->flags << 7) != 128)
+			var->flags |= FLG_ZERO;
 		conv++;
-
 	}
-	while (*conv != var.type)
+	while (*conv != var->type)
 	{
-		if (var.flags < 8 && (ft_isdigit(*conv) || *conv == '*'))
+		if (var->flags < 8 && (ft_isdigit(*conv) || *conv == '*'))
 			// *conv != '0'
-			var.flags |= FLG_WDTH;
-		else if (*conv == '.' && (ft_isdigit(*(conv+1)) || *(conv+1) == '*'))//TODO delete '*'
-			var.flags |= FLG_PRCSN;
+			var->flags |= FLG_WDTH;
+		else if (*conv == '.' && (ft_isdigit(*(conv + 1)) || *(conv + 1) ==
+		'*')) //TODO delete '*'
+			var->flags |= FLG_PRCSN;
 		conv++;
 	}
-	return (var);
 }
 
-t_conf_parser	ft_type_conf(char *conv, t_conf_parser var)
+void	ft_type_conf(char *conv, t_conf_parser *var)
 {
 	char	*type_set;
 	char	*temp;
 	int		i;
 
-	i++;
+	i = 0;
 	type_set = ft_strdup("cspdiuxX%");
 	temp = type_set;
 	while (*conv)
@@ -107,48 +104,59 @@ t_conf_parser	ft_type_conf(char *conv, t_conf_parser var)
 		{
 			if (*conv == type_set[i])
 			{
-				var.type = *conv;
+				var->type = *conv;
 				free(temp);
-				return (var);
+				break;
 			}
 			i++;
 		}
 		i = 0;
 		conv++;
 	}
-	free(temp);
-	return (var); //todo неопределнное поведение
 }
 
-int	ft_var_conf(char *conv, va_list arg_ptr)
+void ft_var_clean(t_conf_parser *var)
 {
-	t_conf_parser	var;
+	var->type = 0;
+	var->flags &= FLG_NONE;
+	var->precision = 0;
+	var->width = 0;
+	var->count = 0; //
+}
 
-	var = ft_type_conf(conv, var);	//todo find type first, skipping all flags
-	var = ft_flag_conf(conv, var);
-	printf("%d", var.flags);
-	if (var.flags >= 3)
-		var = ft_wdth_prcsn(conv, var, arg_ptr);
-	//printf("'%s' --- conv ", conv);
+void	ft_var_conf(char *conv, t_conf_parser *var, va_list arg_ptr)
+{
+	ft_var_clean(var);
+	ft_type_conf(conv, var);	//todo find type first, skipping allflags
+	if (!var->type)
+		exit(0);
+	ft_flag_conf(conv, var);
+	//printf("\n'%d'-'%c'",var->flags, var->type);
+	if (var->flags > (FLG_MINUS | FLG_ZERO))
+	{
+		ft_wdth_prcsn(conv, var, arg_ptr);
+	}
+	//conv++;
+	//printf("\n'%c' --- var-type ", var.type);
 	ft_var_output(var, arg_ptr);
 	//ft_printf(conv, arg_ptr);
-	return (var.count);
 }
 
 int	ft_printf(const char *arg, ...)
 {
 	va_list	arg_ptr;
-	int		count;
 	char	*conv;
-	char	*text;
+	t_conf_parser	var;
 
 	va_start(arg_ptr, arg);
-	conv = ft_strchr(arg, '%');
-	conv++;
 	while (*arg != '%')
 		ft_putchar_fd(*arg++, 1);
-	count = ft_var_conf(conv, arg_ptr);
+	conv = ft_strchr(arg, '%');
+	conv++;
+	//printf("\n'%s' --- conv\n", conv);
+	ft_var_conf(conv, &var, arg_ptr); //TODO передавать conv как указатель
+	//printf("\n'%c' --- type ", var.type);
 	va_end (arg_ptr);
 	//printf("\n*conv -- '%c'\n", *conv);
-	return (count);
+	return (var.count);
 }
--- main.c
+++ main.c
@@ -8,34 +8,34 @@ int	main(void)
 {
 	char			c = 'a';
 	char			*s = "Abdulah Sun !";
-	int				d = 2;
+	int				d = 3002;
 	unsigned int	u = 2147483648;
 
-//	ft_printf("\n'c'\nMine	 -- '%c'", c);
-//	printf("\nOriginal -- '%c'\n", c);
-//
-//	ft_printf("\n's'\nMine	 -- '%s'", s);
-//	printf("\nOriginal -- '%-1000s'\n", s);
-//
-//	ft_printf("\n'p'\nMine	 -- '%p'", &s);
-//	printf("\nOriginal -- '%p'\n", &s);
-//
-	ft_printf("\n'd'\nMine	 -- '%5.8%%d'", d, 10);
-	printf("\nOriginal -- '%10%%d'\n", d);
-//
-//	ft_printf("\n'i'\nMine	 -- '%i'", d);
-//	printf("\nOriginal -- '%i'\n", d);
-//
-//	ft_printf("\n'u'\nMine	 -- '%u'", u);
-//	printf("\nOriginal -- '%u'\n", u);
-//
-//	ft_printf("\n'x'\nMine	 -- '%x'", u);
-//	printf("\nOriginal -- '%x'\n", u);
-//
-//	ft_printf("\n'X'\nMine	 -- '%X'", d);
-//	printf("\nOriginal -- '%X'\n", d);
-//
-//	ft_printf("\nMine	 -- '%%'");
-//	printf("\nOriginal  -- '%%'\n");
+	ft_printf("\n'c'\nMine	 -- '%-c'", c);
+	printf("\nOriginal -- '%c'\n", c);
+
+	ft_printf("\n's'\nMine	 -- '%-s'",  s);
+	printf("\nOriginal -- '%s'\n", s);
+
+	ft_printf("\n'p'\nMine	 -- '%0p'", &s);
+	printf("\nOriginal -- '%p'\n", &s);
+
+	ft_printf("\n'd'\nMine	 -- '%0d'", d);
+	printf("\nOriginal -- '%d'\n", d);
+
+	ft_printf("\n'i'\nMine	 -- '%-i'", d);
+	printf("\nOriginal -- '%i'\n", d);
+
+	ft_printf("\n'u'\nMine	 -- '%0u'", u);
+	printf("\nOriginal -- '%u'\n", u);
+
+	ft_printf("\n'x'\nMine	 -- '%-x'", u);
+	printf("\nOriginal -- '%x'\n", u);
+
+	ft_printf("\n'X'\nMine	 -- '%0X'", d);
+	printf("\nOriginal -- '%X'\n", d);
+
+	ft_printf("\nMine	 -- '%-%'");
+	printf("\nOriginal  -- '%-%'\n");
 	return 0;
 }
