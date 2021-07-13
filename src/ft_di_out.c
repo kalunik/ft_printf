84a725a88ac06c5cb87a45a67fc542ad637c5a93
wjonatho <wjonatho@student.21-school.ru>
2021-07-13 16:25:28 +0300

    Fixed leaks itoa

--- src/ft_di_out.c
+++ src/ft_di_out.c
@@ -2,9 +2,14 @@
 
 void	ft_di_out(t_conf_parser *var, va_list arg_ptr)
 {
-	int	d;
+	int		d;
+	char	*len;
+	char	*tmp;
 
 	d = va_arg(arg_ptr, int);
-	var->count = ft_strlen(ft_itoa(d));
+	len = ft_itoa(d);
+	tmp = len;
+	var->count = ft_strlen(len);
 	ft_putnbr_fd(d, 1);
+	free(tmp);
 }
